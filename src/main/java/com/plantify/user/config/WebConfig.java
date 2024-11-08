f21c363fdca9b18ece3ac1db602874dca3851cc5
seunzu <0123suh@gmail.com>
2024-11-08 23:20:06 +0900

    chore: 초기설정

--- src/main/java/com/plantify/user/config/WebConfig.java
+++ src/main/java/com/plantify/user/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.plantify.chat.config;
+package com.plantify.user.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
