ca3c3ccf1e0c5243e58cb51078deaf56b853be21
zzoo0123 <0123suh@gmail.com>
2024-11-16 18:32:33 +0900

    feat: ActivityLogController

---
+++ src/main/java/com/plantify/admin/controller/ActivityLogController.java
@@ -0,0 +1,40 @@
+package com.plantify.admin.controller;
+
+import com.plantify.admin.domain.dto.request.ActivityLogRequest;
+import com.plantify.admin.domain.dto.response.ActivityLogResponse;
+import com.plantify.admin.domain.entity.TargetType;
+import com.plantify.admin.global.response.ApiResponse;
+import com.plantify.admin.service.ActivityLogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/admin/activity-logs")
+public class ActivityLogController {
+
+    private final ActivityLogService activityLogService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> recordActivityLog(@RequestBody ActivityLogRequest request) {
+        activityLogService.recordActivity(request);
+        return ResponseEntity.ok(ApiResponse.ok());
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ApiResponse<List<ActivityLogResponse>>> getAllActivityLogs() {
+        List<ActivityLogResponse> allActivityLogs = activityLogService.getAllActivityLogs();
+        return ResponseEntity.ok(ApiResponse.ok(allActivityLogs));
+    }
+
+    @GetMapping("/target")
+    public ResponseEntity<ApiResponse<List<ActivityLogResponse>>> getActivityLogsByTarget(
+            @RequestParam TargetType targetType,
+            @RequestParam Long targetId) {
+        List<ActivityLogResponse> activityLogs = activityLogService.getActivityLogs(targetType, targetId);
+        return ResponseEntity.ok(ApiResponse.ok(activityLogs));
+    }
+}
