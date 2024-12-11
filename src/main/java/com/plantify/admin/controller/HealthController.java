fd746f4089fca0dd99d1bd0d275c3c3f8a216f39
seunzu <0123suh@gmail.com>
2024-12-11 12:34:26 +0900

    feat: healthCheck

--- src/main/java/com/plantify/admin/controller/HealthController.java
+++ src/main/java/com/plantify/admin/controller/HealthController.java
@@ -1,10 +1,12 @@
 package com.plantify.admin.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
+    @GetMapping
     public String health() {
         return "OK";
     }
