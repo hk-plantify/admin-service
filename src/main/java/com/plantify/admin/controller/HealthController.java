c18bdefa3ae4a61b46adde9925bd361554c02c77
seunzu <0123suh@gmail.com>
2024-12-12 02:04:15 +0900

    feat: uri 수정

--- src/main/java/com/plantify/admin/controller/HealthController.java
+++ src/main/java/com/plantify/admin/controller/HealthController.java
@@ -1,13 +1,26 @@
 package com.plantify.admin.controller;
 
+import com.plantify.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthController {
+
+    private final UserService userService;
 
     @GetMapping
     public String health() {
         return "OK";
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Long> getUserId(@RequestParam String username) {
+        Long userId = userService.getUserId(username);
+        return ResponseEntity.ok(userId);
     }
 }
